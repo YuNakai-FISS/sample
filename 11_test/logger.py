87df2a453d46fb031c300405846592376de4184e
ynakai <y-nakai@fissc.co.jp>
2026-04-09 10:53:37 +0900

    L11 C5.3 モックを作成しテストするコード

---
+++ 11_test/logger.py
@@ -0,0 +1,3 @@
+class Logger:
+    def log(self, message: str):
+        raise NotImplementedError("log meth not implemented")
--- 11_test/test_calculator.py
+++ 11_test/test_calculator.py
@@ -1,11 +1,15 @@
 import pytest
 from calculator import Calculator
+from unittest.mock import MagicMock
+from logger import Logger
 
 @pytest.fixture
 def calculator():
-    calc = Calculator()
+    # Loggerをモックに置き換える
+    mock_logger = MagicMock(spec=Logger)
+    calc = Calculator(logger=mock_logger)
     yield calc
-    calc.reset
+    calc.reset()
 
 # 各メソッドのテスト
 @pytest.mark.parametrize("initial, add_value, expected", [
@@ -17,6 +21,7 @@ def test_add(calculator, initial, add_value, expected):
     calculator.add(initial)
     calculator.add(add_value)
     assert calculator.result == expected
+    calculator.logger.log.assert_called()
 
 @pytest.mark.parametrize("initial, sub_value, expected", [
     (10, 3, 7),
@@ -27,6 +32,7 @@ def test_subtract(calculator, initial, sub_value, expected):
     calculator.add(initial)
     calculator.subtract(sub_value)
     assert calculator.result == expected
+    calculator.logger.log.assert_called()
 
 @pytest.mark.parametrize("initial, mul_value, expected", [
     (1, 3, 3),
@@ -37,6 +43,7 @@ def test_multiply(calculator, initial, mul_value, expected):
     calculator.add(initial)
     calculator.multiply(mul_value)
     assert calculator.result == expected
+    calculator.logger.log.assert_called()
 
 @pytest.mark.parametrize("initial, div_value, expected", [
     (10, 2, 5),
@@ -47,11 +54,13 @@ def test_divide(calculator, initial, div_value, expected):
     calculator.add(initial)
     calculator.divide(div_value)
     assert calculator.result == expected
+    calculator.logger.log.assert_called()
 
 def test_divide_by_zero(calculator):
     calculator.add(10)
     with pytest.raises(ValueError, match="division error"):
         calculator.divide(0)
+    calculator.logger.log.assert_called_with("Divide(0): error = division by zero")
 
 # 複雑な計算のテスト
 def test_complex_calculation(calculator):
@@ -60,3 +69,4 @@ def test_complex_calculation(calculator):
     calculator.subtract(1)
     calculator.divide(2)
     assert calculator.result == 2.5
+    calculator.logger.log.assert_called()
